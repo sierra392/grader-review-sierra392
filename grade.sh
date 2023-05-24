f0e4415029bfafda4b96b555d22d856b92be7385
Sierra <130005818+sierra392@users.noreply.github.com>
2023-05-24 16:45:07 -0700

    update

--- grade.sh
+++ grade.sh
@@ -1,5 +1,4 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
-
+CPATH=".;../lib/hamcrest-core-1.3.jar;../lib/junit-4.13.2.jar"
 rm -rf student-submission
 rm -rf grading-area
 
@@ -14,3 +13,38 @@ echo 'Finished cloning'
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+
+if [[ -f student-submission/ListExamples.java ]]
+    then 
+        echo 'File found'
+
+else
+    echo 'FILE NOT FOUND!'
+    exit
+fi
+cp -r *.java grading-area
+cp -r student-submission/*.java grading-area
+
+if [[ $? != 0 ]]
+then 
+    echo 'compile failed'
+    exit
+else
+    echo 'compile success'
+fi
+
+cd grading-area
+javac -cp $CPATH *.java
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > output.txt
+
+result=$(grep -c 'OK' output.txt)
+fails=$(grep -c -i 'caused by' output.txt)
+
+if [[ $result > 0 ]]
+then 
+    echo 'all tests passed'
+    exit
+else
+    echo 'tests failed'
+fi
+echo $fails
